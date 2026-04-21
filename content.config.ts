c78bb737898ecee24d46e7eac5e0b94135266bcb
th0h0e <thmasthe@gmail.com>
2026-04-21 13:50:22 +0200

    good

--- content.config.ts
+++ content.config.ts
@@ -1,6 +1,31 @@
-import { defineContentConfig } from '@nuxt/content'
-import { collections } from './types/content'
+import { defineContentConfig, defineCollection } from '@nuxt/content'
 
 export default defineContentConfig({
-  collections
+  collections: {
+    collections: defineCollection({
+      type: 'data',
+      source: 'collections/**/*.yaml',
+      schema: {
+        title: { type: 'string', required: true },
+        description: { type: 'string', required: true },
+        language: { type: 'string', required: true },
+        icon: { type: 'string', required: false },
+        color: { type: 'string', required: false }
+      }
+    }),
+    vocabCards: defineCollection({
+      type: 'data',
+      source: 'vocab/**/*.yaml',
+      schema: {
+        collection: { type: 'string', required: true },
+        term: { type: 'string', required: true },
+        definition: { type: 'string', required: true },
+        pinyin: { type: 'string', required: false },
+        example: { type: 'string', required: false },
+        tags: { type: 'array', required: false },
+        difficulty: { type: 'string', required: false },
+        order: { type: 'number', required: false }
+      }
+    })
+  }
 })
